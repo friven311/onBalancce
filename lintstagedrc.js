6c99741823ec83936e26753b9c6142bd0ec7981b
Viktor <mr.dyrban@gmail.com>
2022-11-19 00:30:03 +0300

    setting the app

---
+++ lintstagedrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Any application code written in TS (including styled-components)
+    '*.ts?(x)': [
+      'prettier --write --cache',
+      'eslint --fix --cache',
+      'stylelint --fix --cache',
+      () => 'tsc -p tsconfig.json --noEmit',
+    ],
+    // Any supporting JS code (scripts, configs, etc.)
+    '*.js?(x)': ['prettier --write --cache', 'eslint --fix --cache'],
+    // All the rest files should follow prettier formatting
+    '!((*.ts?(x))|(*.js?(x)))': 'prettier --ignore-unknown --write --cache',
+  };
+  Footer
+  
